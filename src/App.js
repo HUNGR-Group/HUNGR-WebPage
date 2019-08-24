8150e7dfd881c0824fbf7c93494b7dfdfdc03c0d
Adrian Garcia <adriangarlop@hotmail.com>
2019-08-24 14:58:04 -0500

    Modified app.css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function initialContent() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1></h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,18 @@ function App() {
         >
           Learn React
         </a>
+      </header>
+    </div>
+  );
+}
+
+// <img src=".src/assets/logo.jpg"></img>
+
+function App() {
+  return (
+    <div class="content">
+      <header className="header">
+                
       </header>
     </div>
   );
