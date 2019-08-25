86033279409159602a75509fa24529d11094e057
Adrian Garcia <adriangarlop@hotmail.com>
2019-08-24 20:21:53 -0500

    Changed laptop

--- src/App.js
+++ src/App.js
@@ -33,30 +33,59 @@ function Header() {
       </header>
       <div class="places">
         <table class="table table-hover">
-          <thead>
-            <tr>
-              <th>Mas</th>
-              <th>Lugar</th>
-              <th>Comidas disponibles</th>
-            </tr>
-          </thead>
           <tbody>
             <tr>
               <td>
+                <h3 class="top">Titos</h3>
+              </td>
+              <td>
+                <img class="small" src="https://img1.wsimg.com/isteam/ip/c6543e0e-1bb7-4875-ae65-b220bfb8aac4/logo/Titos%20Roja.png/:/rs=h:125/qt=q:95"></img>
+              </td>
+              <td>
+                <ul>
+                  <li>Hamburguesas</li>
+                  <li>Alitas</li>
+                  <li>Boneless</li>
+                  <li>Tacos</li>
+                  <li>Burritos</li>
+                </ul>
+              </td>
+              <td>
+                asd
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3 class="top">KFC</h3>
+              </td>
+              <td>
+                <img class="KFC" src="https://i.pinimg.com/originals/aa/92/89/aa9289de1ed2865bccd7c7457f246482.jpg"></img>                
+              </td>
+              <td>
+                <ul>
+                  <li>Hamburguesas</li>
+                  <li>Alitas</li>
+                  <li>Boneless</li>
+                </ul>
+              </td>
+              <td>
                 
               </td>
-              <td>Doe</td>
-              <td>john@example.com</td>
             </tr>
             <tr>
-              <td>Mary</td>
-              <td>Moe</td>
-              <td>mary@example.com</td>
-            </tr>
-            <tr>
-              <td>July</td>
-              <td>Dooley</td>
-              <td>july@example.com</td>
+              <td>
+                <h3 class="top">McDonald's</h3>
+              </td>
+              <td>
+                <img class="small" src="https://logosmarcas.com/wp-content/uploads/2018/05/McDonalds-S%C3%ADmbolo.png"></img>                
+              </td>
+              <td>
+                <ul>
+                  <li>Hamburguesas</li>
+                </ul>
+              </td>
+              <td>
+              </td>
             </tr>
           </tbody>
         </table>       
